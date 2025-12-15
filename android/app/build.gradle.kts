b4664b6f5349266889753bbb996cb63784876bf2
Modacher Mahmud Rafi <modacher.mahmud.rafi@gmail.com>
2025-12-16 01:07:59 +0600

    Fvm created and others folder are created.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.scube_task"
+    namespace = "com.scube.task"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.scube_task"
+        applicationId = "com.scube.task"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/scube/task/MainActivity.kt
+++ android/app/src/main/kotlin/com/scube/task/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.scube_task
+package com.scube.task
 
 import io.flutter.embedding.android.FlutterActivity
 
